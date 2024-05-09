97e280c9b5f7c94c739597c849a606dc7dbf84c0
Dmitrii Los <dimaconway@gmail.com>
2024-05-09 17:55:05 +0300

    contacts before delete

--- cmd/htmx/main.go
+++ cmd/htmx/main.go
@@ -3,28 +3,99 @@ package main
 import (
 	"fmt"
 	"html/template"
+	"log"
 	"net/http"
 )
 
-type Count struct {
-	Count int
+type Contact struct {
+	Name  string
+	Email string
+}
+
+type Contacts = []Contact
+
+type Data struct {
+	Contacts Contacts
+}
+
+func (d *Data) hasEmail(email string) bool {
+	for _, contact := range d.Contacts {
+		if contact.Email == email {
+			return true
+		}
+	}
+	return false
+}
+
+func newData() Data {
+	return Data{
+		Contacts: []Contact{
+			{Name: "first", Email: "first@"},
+			{Name: "second", Email: "second@"},
+			{Name: "third", Email: "third@"},
+		},
+	}
+}
+
+type FormData struct {
+	Values map[string]string
+	Errors map[string]string
+}
+
+func newFormData() FormData {
+	return FormData{
+		Values: make(map[string]string),
+		Errors: make(map[string]string),
+	}
+}
+
+type Page struct {
+	Data Data
+	Form FormData
+}
+
+func newPage() Page {
+	return Page{
+		Data: newData(),
+		Form: newFormData(),
+	}
 }
 
 func main() {
 	templates := template.Must(template.ParseGlob("views/*.html"))
 
-	count := &Count{Count: 0}
+	page := newPage()
 
 	router := http.NewServeMux()
 	router.HandleFunc("GET /", func(w http.ResponseWriter, r *http.Request) {
-		if err := templates.ExecuteTemplate(w, "index", count); err != nil {
+		if err := templates.ExecuteTemplate(w, "index", page); err != nil {
 			http.Error(w, err.Error(), http.StatusInternalServerError)
 		}
 	})
-	router.HandleFunc("POST /count", func(w http.ResponseWriter, r *http.Request) {
-		count.Count++
+	router.HandleFunc("POST /contacts", func(w http.ResponseWriter, r *http.Request) {
+		name := r.FormValue("name")
+		email := r.FormValue("email")
 
-		if err := templates.ExecuteTemplate(w, "count", count); err != nil {
+		if page.Data.hasEmail(email) {
+			formData := newFormData()
+			formData.Values["name"] = name
+			formData.Values["email"] = email
+			formData.Errors["email"] = "email already exists"
+
+			w.WriteHeader(http.StatusUnprocessableEntity)
+			if err := templates.ExecuteTemplate(w, "form", formData); err != nil {
+				http.Error(w, err.Error(), http.StatusInternalServerError)
+			}
+			return
+		}
+
+		contact := Contact{name, email}
+		page.Data.Contacts = append(page.Data.Contacts, contact)
+
+		if err := templates.ExecuteTemplate(w, "form", newFormData()); err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+		}
+		if err := templates.ExecuteTemplate(w, "oob-contact", contact); err != nil {
 			http.Error(w, err.Error(), http.StatusInternalServerError)
 		}
 	})
@@ -34,5 +105,5 @@ func main() {
 		Handler: router,
 	}
 	fmt.Println("Starting server on port :8080")
-	server.ListenAndServe()
+	log.Fatal(server.ListenAndServe())
 }
